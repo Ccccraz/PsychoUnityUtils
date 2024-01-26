69c814c1e7690b54acff4ba9cdbf5ada8f7a358b
Ccccraz <hy.ccccr@gmail.com>
2024-01-27 02:36:40 +0800

    test about nameof

---
+++ NameofTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace IOTest;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var dataName = new Vector3
+        {
+            X = 0,
+            Y = 1,
+            Z = 2
+        };
+        
+        Console.WriteLine($"X: {dataName.X}, Y: {dataName.Y}, Z: {dataName.Z}");
+
+        Console.WriteLine($"{nameof(dataName)}");
+    }
+}
